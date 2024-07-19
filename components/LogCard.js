7472d6ecf3db2e2d033d024f9d00c359db9105fc
Tatiana Barga <tatibspersonal@gmail.com>
2024-07-18 20:19:06 -0500

    profiles load on user page and profile view displays profile info, log skeleton code started on profile page

---
+++ components/LogCard.js
@@ -0,0 +1,49 @@
+import Link from 'next/link';
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+export default function LogCard({ logObj }) {
+  return (
+    <div>
+      <Card className="text-center">
+        <Card.Text>{logObj.title}</Card.Text>
+        <Card.Text>{logObj.score_impact}</Card.Text>
+        <Card.Text>{logObj.description}</Card.Text>
+        <Card.Text>{logObj.event_date}</Card.Text>
+        <Card.Text>{logObj.log_date}</Card.Text>
+        <Card.Body>
+
+          {/* TODO: create new form functionality */}
+          {/* <Link href={`/logs/new`} passHref>
+            <Button variant="primary" className="m-2">
+              Create New
+            </Button>
+          </Link> */}
+
+          {/* TODO: single log view functionality */}
+          <Link href={`/logs/${logObj.id}`} passHref>
+            <Button variant="primary" className="m-2">
+              View Log
+            </Button>
+          </Link>
+
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+LogCard.propTypes = {
+  logObj: PropTypes.shape({
+    id: PropTypes.string,
+    score_impact: PropTypes.string,
+    creator_id: PropTypes.string,
+    description: PropTypes.string,
+    event_date: PropTypes.string,
+    log_date: PropTypes.string,
+    profile_id: PropTypes.string,
+    title: PropTypes.string,
+  }).isRequired,
+  // onUpdate: PropTypes.func.isRequired,
+};
--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -2,8 +2,13 @@ import Link from 'next/link';
 // import React, { useEffect, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
 export default function ProfileCard({ profileObj }) {
+  useEffect(() => {
+    console.log(profileObj);
+  });
+
   return (
     <div>
       <Card className="text-center">
@@ -35,7 +40,7 @@ ProfileCard.propTypes = {
   profileObj: PropTypes.shape({
     name: PropTypes.string,
     score: PropTypes.string,
-    id: PropTypes.string,
+    id: PropTypes.number,
   }).isRequired,
   // onUpdate: PropTypes.func.isRequired,
 };
