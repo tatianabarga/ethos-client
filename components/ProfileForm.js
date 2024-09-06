2d7e761d5f6557de3e85ed937b68a29b639e7201
Tatiana Barga <tatibspersonal@gmail.com>
2024-09-06 15:12:26 -0500

    initial score feild only renders on create not on update

--- components/ProfileForm.js
+++ components/ProfileForm.js
@@ -87,10 +87,14 @@ function ProfileForm({ obj }) {
           <Form.Control type="text" value={formInput.bio} placeholder="Who is this person/ corporation/ contractor? How are they involved? How are they related to other relevant profiles? Is there anything else circle members will need to know?" name="bio" onChange={handleChange} />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="initialScore">
-          <Form.Label>Initial Score</Form.Label>
-          <Form.Control type="text" placeholder="You can give them an initial score here. This is optional." name="initial_score" onChange={handleChange} />
-        </Form.Group>
+        <>
+          {obj.id ? null : (
+            <Form.Group className="mb-3" controlId="initialScore">
+              <Form.Label>Initial Score</Form.Label>
+              <Form.Control type="text" placeholder="You can give them an initial score here. This is optional." name="initial_score" onChange={handleChange} />
+            </Form.Group>
+          )}
+        </>
 
         <Form.Label>What circles do you want this profile to be shared with?</Form.Label>
         <ToggleButtonGroup type="checkbox" className="mb-2">
