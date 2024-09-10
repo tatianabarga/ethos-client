eb140ce06a3f5c5c72a78400315256a6cd864915
Tatiana Barga <tatibspersonal@gmail.com>
2024-09-10 12:37:19 -0500

    circle form create function tested and working

---
+++ pages/circles/new.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CircleForm from '../../components/CircleForm';
+
+export default function newCircle() {
+  return (
+    <div>
+      Create New Circle
+      <CircleForm />
+    </div>
+  );
+}
--- utils/data/circleData.js
+++ utils/data/circleData.js
@@ -48,9 +48,41 @@ const getSingleCircle = (id) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const createCircle = (payload) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/circles`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateCircle = (payload) => new Promise((resolve, reject) => {
+  if (!payload.id) {
+    reject(new Error('Circle ID is required for updating a circle.'));
+    return;
+  }
+
+  fetch(`${clientCredentials.databaseURL}/circles/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
 export {
   getAllCircles,
   getCirclesByProfile,
   getCirclesByUser,
   getSingleCircle,
+  createCircle,
+  updateCircle,
 };
