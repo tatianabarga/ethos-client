fde0c6587f7025e4ed9df97938f7b59e67fae961
Tatiana Barga <tatibspersonal@gmail.com>
2024-09-16 18:35:29 -0500

    put score on profile card

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -2,13 +2,27 @@ import Link from 'next/link';
 // import React, { useEffect, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import getScoreByProfile from '../utils/data/scoreData';
 
 export default function ProfileCard({ profileObj }) {
+  const [score, setScore] = useState(null);
+
+  useEffect(() => {
+    if (profileObj.id) {
+      getScoreByProfile(profileObj.id).then((data) => {
+        if (data && data.length > 0 && data[0].score) {
+          setScore(data[0].score);
+        }
+      });
+    }
+  }, [profileObj]);
+
   return (
     <div>
       <Card className="text-center">
         <Card.Text className="subheader-card">{profileObj.name}</Card.Text>
-        <Card.Text>{profileObj.score}</Card.Text>
+        <Card.Text className="score">{score}</Card.Text>
         <Card.Body>
 
           {/* TODO: create new form functionality */}
