29938f040994fd38bc7af9a7337f24212030bac5
Tatiana Barga <tatibspersonal@gmail.com>
2024-09-17 16:56:52 -0500

    fixed method of adding users to circle to be more practical

--- components/CircleForm.js
+++ components/CircleForm.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+// import { ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { useAuth } from '../utils/context/authContext';
 import { getAllUsers, getUsersByCircle } from '../utils/data/userData';
@@ -18,6 +18,8 @@ function CircleForm({ obj }) {
   const [users, setUsers] = useState([]);
   const [formInput, setFormInput] = useState(initialState);
   const [selectedUsers, setSelectedUsers] = useState([]);
+  const [selectedUserDetails, setSelectedUserDetails] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
   const router = useRouter();
   const { user } = useAuth();
 
@@ -29,16 +31,16 @@ function CircleForm({ obj }) {
     }));
   };
 
-  const handleToggleUser = (userId) => {
-    setSelectedUsers((prevSelectedUsers) => {
-      let newUsers = [];
-      if (prevSelectedUsers.includes(userId)) {
-        newUsers = prevSelectedUsers.filter((id) => id !== userId);
-      } else {
-        newUsers = [...prevSelectedUsers, userId];
-      }
-      return newUsers;
-    });
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSelectUser = (userId) => {
+    setSelectedUsers((prevSelectedUsers) => (
+      prevSelectedUsers.includes(userId)
+        ? prevSelectedUsers.filter((id) => id !== userId)
+        : [...prevSelectedUsers, userId]
+    ));
   };
 
   const handleSubmit = (e) => {
@@ -60,9 +62,7 @@ function CircleForm({ obj }) {
 
   useEffect(() => {
     getAllUsers().then(setUsers);
-    if (selectedUsers.includes(user.id)) {
-      // do nothing
-    } else {
+    if (!selectedUsers.includes(user.id)) {
       selectedUsers.push(user.id);
     }
   }, [selectedUsers, user]);
@@ -91,6 +91,14 @@ function CircleForm({ obj }) {
     }));
   }, [user]);
 
+  useEffect(() => {
+    setSelectedUserDetails(users.filter((includedUser) => selectedUsers.includes(includedUser.id)));
+  }, [selectedUsers, users]);
+
+  const filteredUsers = users.filter(
+    (thisUser) => thisUser.name.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+
   return (
     <div>
       <Form className="subheader-card" onSubmit={handleSubmit}>
@@ -99,26 +107,51 @@ function CircleForm({ obj }) {
           <Form.Control className="input body-text" type="text" value={formInput.name} placeholder="Name this circle." name="name" onChange={handleChange} />
         </Form.Group>
 
-        <Form.Label>What users do you want this circle to be shared with?</Form.Label>
-        <ToggleButtonGroup
-          type="checkbox"
-          value={selectedUsers}
-          className="toggles"
-        >
-          {users.map((thisUser) => (
-            <ToggleButton
-              className="toggles"
-              key={thisUser.id}
-              id={`user-${thisUser.id}`}
-              variant="outline-primary"
-              value={thisUser.id}
-              checked={selectedUsers.includes(thisUser.id)}
-              onChange={() => handleToggleUser(thisUser.id)}
-            >
-              {thisUser.name}
-            </ToggleButton>
+        <Form.Group className="mb-3" controlId="search">
+          <Form.Label className="sm-margin">What users do you want this circle to be shared with?</Form.Label>
+          <div className="body-card sm-margin">Search for users</div>
+          <Form.Control
+            className="input body-text"
+            type="text"
+            placeholder="Search users by name"
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+        </Form.Group>
+
+        <ul className="list-group">
+          {searchQuery !== '' ? filteredUsers.map((thisUser) => (
+            <li key={thisUser.id} className="list-group-item">
+              <div className="d-flex justify-content-between align-items-center">
+                <span>{thisUser.name}</span>
+                <Button
+                  variant={selectedUsers.includes(thisUser.id) ? 'danger' : 'primary'}
+                  onClick={() => handleSelectUser(thisUser.id)}
+                >
+                  {selectedUsers.includes(thisUser.id) ? 'Remove' : 'Add'}
+                </Button>
+              </div>
+            </li>
+          ))
+            : null}
+        </ul>
+
+        <div className="abv-margin sm-margin subheader-card">Users in this Circle</div>
+        <ul className="list-group">
+          {selectedUserDetails.map((thisUser) => (
+            <li key={thisUser.id} className="list-group-item">
+              <div className="d-flex justify-content-between align-items-center">
+                <span>{thisUser.name}</span>
+                <Button
+                  variant={selectedUsers.includes(thisUser.id) ? 'danger' : 'primary'}
+                  onClick={() => handleSelectUser(thisUser.id)}
+                >
+                  {selectedUsers.includes(thisUser.id) ? 'Remove' : 'Add'}
+                </Button>
+              </div>
+            </li>
           ))}
-        </ToggleButtonGroup>
+        </ul>
 
         <Button variant="primary" type="submit">
           Submit
