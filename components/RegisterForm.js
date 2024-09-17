cafd9180e5467fbb2b352c659b9fd9d4537b4531
Tatiana Barga <tatibspersonal@gmail.com>
2024-09-17 17:21:21 -0500

    register form also prevents users from creating the same name as someone else

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { registerUser } from '../utils/auth';
+import { getAllUsers } from '../utils/data/userData';
 
 function RegisterForm({ user, updateUser }) {
+  const [existingUsers, setExistingUsers] = useState([]);
   const [formData, setFormData] = useState({
     name: '',
     uid: user.uid,
@@ -12,11 +14,23 @@ function RegisterForm({ user, updateUser }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // check if user with name input already exists
+    const nameExists = existingUsers.some((existingUser) => existingUser.name.toLowerCase() === formData.name.toLowerCase());
+
+    if (nameExists) {
+      alert('A user with this name already exists!');
+      return; // Prevent form submission
+    }
+
     registerUser(formData).then(() => updateUser(user.uid));
   };
 
+  useEffect(() => {
+    getAllUsers().then((data) => { setExistingUsers(data); });
+  });
+
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form className="subheader-card" onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className="subheader-card">Your Name</Form.Label>
         <Form.Control className="subheader-card" as="textarea" name="name" required placeholder="Enter your Name" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
